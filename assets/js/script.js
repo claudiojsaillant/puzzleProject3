061e7d169168156e31e62557335ee5bfc4f3195e
Claudio Saillant <claudiojsaillant@gmail.com>
2020-11-27 17:51:34 -0500

    Adding comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,34 +17,43 @@ const elementsSwapper = (obj1, obj2) => {
 };
 
 const winCheck = () => {
+  // Making current string from main ARR
   let myStr = "";
   MAIN_ARR.forEach((el) => {
     myStr += el.join("");
   });
+  // Comparing current string vs winString created when the table was also created. 
   return myStr === winStr;
 };
 
 const swapNeighbor = (spaceEl, currEl) => {
+  // Getting the row/col atrribute from the current element
   let tempRow = currEl.getAttribute("row");
   let tempCol = currEl.getAttribute("col");
+  // Swapping same elements in MAIN ARR so we can keep track of the squares
   let temp = MAIN_ARR[spaceEl.getAttribute("row")][spaceEl.getAttribute("col")];
   MAIN_ARR[spaceEl.getAttribute("row")][spaceEl.getAttribute("col")] = MAIN_ARR[tempRow][tempCol];
   MAIN_ARR[tempRow][tempCol] = temp;
+  // Swapping both elements attributes
   currEl.setAttribute("col", spaceEl.getAttribute("col"));
   currEl.setAttribute("row", spaceEl.getAttribute("row"));
   spaceEl.setAttribute("col", tempCol);
   spaceEl.setAttribute("row", tempRow);
+  // Finnally swapping the elements
   elementsSwapper(currEl, spaceEl);
 };
 
 const shuffleTable = () => {
   for (let h = 0; h < 1000; h++) {
+    // Getting space el
     let spaceEl = document.getElementsByClassName("space")[0];
+    // Pull space neighbors
     let neighbors = spaceNeighbors(
       spaceEl.getAttribute("row"),
       spaceEl.getAttribute("col"),
       MAIN_ARR
     );
+    // Swap space with random neighbor
     let randomIndex = Math.floor(Math.random() * neighbors.length);
     let currEl = document.getElementById("cube" + neighbors[randomIndex]);
     swapNeighbor(spaceEl, currEl);
@@ -53,7 +62,9 @@ const shuffleTable = () => {
 
 
 const pictureChanger = (img) => {
+  // Setting url pic for each cube
   let imgUrl = "url(" + img + ")";
+  // Background position for each cube
   let positions = [
     "0px 0px",
     "-100px 0px",
@@ -71,6 +82,7 @@ const pictureChanger = (img) => {
     "-100px -300px",
     "-200px -300px",
   ];
+  //Setting background + position for each cube
   for (let i = 1; i < 16; i++) {
     let currentEl = document.getElementById("cube" + i);
     currentEl.style.backgroundImage = imgUrl;
@@ -80,18 +92,23 @@ const pictureChanger = (img) => {
 
 const tableCreator = (row, col) => {
   let count = 0;
+  // Getting table element
   let table = document.getElementById("myTable");
+  // Making sure its empty before appending childs
   table.innerHTML = "";
   let arr = [];
   for (let i = 0; i < row; i++) {
+    // Create new row
     let newRow = document.createElement("tr");
     let arrRow = [];
     newRow.setAttribute("id", "row" + (i + 1));
     for (let g = 0; g < col; g++) {
+      // Keeping track of all the squares created
       count++;
-
+      // Creating data for each row
       let newCol = document.createElement("td");
       newCol.innerText = count;
+      // Make the last element the space
       if (count == row * col) {
         newCol.innerText = "";
         newCol.setAttribute("class", "space");
@@ -99,6 +116,7 @@ const tableCreator = (row, col) => {
       } else {
         arrRow.push(count);
       }
+      // Atributes used to keep track of the cubes with --> MAIN_ARR indexes
       newCol.setAttribute("value", count);
       newCol.setAttribute("id", "cube" + count);
       newCol.setAttribute("row", i);
@@ -108,6 +126,7 @@ const tableCreator = (row, col) => {
     arr.push(arrRow);
     table.appendChild(newRow);
   }
+  // Creating win string for later comparisons
   winStr = "";
   MAIN_ARR = arr;
   MAIN_ARR.forEach((el) => {
@@ -116,6 +135,7 @@ const tableCreator = (row, col) => {
 };
 
 let spaceNeighbors = (i, j, arr) => {
+  // return all existing neighbors from space if exist
   let myNeigs = [];
   i = parseInt(i);
   j = parseInt(j);
@@ -140,6 +160,7 @@ let spaceNeighbors = (i, j, arr) => {
 };
 
 const checkIndex = (i, j, arr) => {
+  // Just an index checker to see if an element exist in arr (spaceNeighbor helper)
   let myI = arr.length;
   if (myI <= i || i < 0) {
     return false;
@@ -152,37 +173,47 @@ const checkIndex = (i, j, arr) => {
 };
 
 document.addEventListener("click", (event) => {
+  // Make sure we are clicking a cube
   const isClickable = event.target.nodeName === "TD";
   if (isClickable) {
     let currEl = event.target;
     let currElValue = parseInt(currEl.getAttribute("value"));
     let spaceEl = document.getElementsByClassName("space")[0];
+    // Get current space neighbors
     let neighbors = spaceNeighbors(
       spaceEl.getAttribute("row"),
       spaceEl.getAttribute("col"),
       MAIN_ARR
     );
-
+    // if the cube clicked is a neighbor
     if (neighbors.includes(currElValue)) {
+      // swap space and element
       swapNeighbor(spaceEl, currEl);
+      // also check if the use won after the swap 
       if (winCheck()) {
         alert("You won");
       }
-    } else {
+    } else { // current square is not a neighbor so it can't be moved.
       // alert("Can't swap elements that are not neighbors with the space");
     }
   }
 });
+
+// Hiding shuffle button until game is created
 document.getElementById("shuffle").style.visibility = "hidden";
 
+//Click start button
 document.getElementById("startBtn").addEventListener("click", () => {
   console.log("Click the btn!");
-  let el = document.getElementById("sizes");
+  let el = document.getElementById("sizes"); 
+  // Get user's selected dimension from dropdown
   let rows = parseInt(el.options[el.selectedIndex].getAttribute("row"));
   let cols = parseInt(el.options[el.selectedIndex].getAttribute("col"));
+  // Validate input
   if (rows === NaN || cols === NaN) {
     alert("Rows and Cols have to be a integer number.");
   } else {
+    // All images available
     let imgObj = {
       1: "./assets/img/apple.png",
       2: "./assets/img/elephant.png",
@@ -191,17 +222,22 @@ document.getElementById("startBtn").addEventListener("click", () => {
     }
   
     if(cols === 4){
+      // Get which image the use wants
       image = prompt("Images available: Insert 1 for apple, 2 for elephant, 3 for mario and 4 for react");
+      // Create the table with the dimesions the user gave (4 x 4)
       tableCreator(rows, cols);
+      // Change the picture because the dimesion is 4 x 4 
       pictureChanger(imgObj[parseInt(image)]);
     } else {
+      // Create the table with the dimesions the user gave (8 x 8, 10 x 10, 12 x 12)
       tableCreator(rows, cols);
     }
-
+    // Make shuffle button visible since the table is created
     document.getElementById("shuffle").style.visibility = "visible";
   }
 });
 
+// Call shuffle function everytime shuffle button is clicked
 document.getElementById("shuffle").addEventListener("click", () => {
   shuffleTable();
 });
