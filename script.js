b2d662c57c8f443ed0862cb8042e7d8ce1a103a5
Claudio Saillant <claudiojsaillant@gmail.com>
2020-11-27 12:05:12 -0500

    image support

--- script.js
+++ script.js
@@ -14,79 +14,107 @@ const elementsSwapper = (obj1, obj2) => {
 
   // remove temporary marker node
   temp.parentNode.removeChild(temp);
-}
+};
 
 const winCheck = () => {
-    let myStr = ""
-    MAIN_ARR.forEach(el => {
-      myStr += el.join("");
-    })
-    return myStr === winStr;
-}
-
+  let myStr = "";
+  MAIN_ARR.forEach((el) => {
+    myStr += el.join("");
+  });
+  return myStr === winStr;
+};
 
 const swapNeighbor = (spaceEl, currEl) => {
   let tempRow = currEl.getAttribute("row");
   let tempCol = currEl.getAttribute("col");
   temp = MAIN_ARR[spaceEl.getAttribute("row")][spaceEl.getAttribute("col")];
-  MAIN_ARR[spaceEl.getAttribute("row")][spaceEl.getAttribute("col")] = MAIN_ARR[tempRow][tempCol]
+  MAIN_ARR[spaceEl.getAttribute("row")][spaceEl.getAttribute("col")] =
+    MAIN_ARR[tempRow][tempCol];
   MAIN_ARR[tempRow][tempCol] = temp;
   currEl.setAttribute("col", spaceEl.getAttribute("col"));
   currEl.setAttribute("row", spaceEl.getAttribute("row"));
   spaceEl.setAttribute("col", tempCol);
   spaceEl.setAttribute("row", tempRow);
   elementsSwapper(currEl, spaceEl);
-}
+};
 
 const shuffleTable = () => {
-  for(let h = 0; h < 1000; h++){
+  for (let h = 0; h < 1000; h++) {
     let spaceEl = document.getElementsByClassName("space")[0];
-    let neighbors = spaceNeighbors(spaceEl.getAttribute("row"), spaceEl.getAttribute("col"), MAIN_ARR);
+    let neighbors = spaceNeighbors(
+      spaceEl.getAttribute("row"),
+      spaceEl.getAttribute("col"),
+      MAIN_ARR
+    );
     let randomIndex = Math.floor(Math.random() * neighbors.length);
     let currEl = document.getElementById("cube" + neighbors[randomIndex]);
     swapNeighbor(spaceEl, currEl);
   }
-}
+};
+
+
+const pictureChanger = (img) => {
+  let imgUrl = "url(" + img + ")";
+  let positions = [
+    "0px 0px",
+    "-100px 0px",
+    "-200px 0px",
+    "-300px 0px",
+    "0px -100px",
+    "-100px -100px",
+    "-200px -100px",
+    "-300px -100px",
+    "0px -200px",
+    "-100px -200px",
+    "-200px -200px",
+    "-300px -200px",
+    "0px -300px",
+    "-100px -300px",
+    "-200px -300px",
+  ];
+  for (let i = 1; i < 16; i++) {
+    let currentEl = document.getElementById("cube" + i);
+    currentEl.style.backgroundImage = imgUrl;
+    currentEl.style.backgroundPosition = positions[i - 1];
+  }
+};
 
 const tableCreator = (row, col) => {
-    
-    let count = 0;
-    let table = document.getElementById("myTable");
-    table.innerHTML = "";
-    let arr = [];
-    for(let i = 0; i < row; i++){
-        
-        let newRow = document.createElement("tr");
-        let arrRow = [];
-        newRow.setAttribute("id", "row" + (i+1));
-        for(let g = 0; g < col; g++){
-            count++;
-          
-            let newCol = document.createElement("td");
-            newCol.innerText = count;
-            if(count == row * col){
-                newCol.innerText = "";
-                newCol.setAttribute("class", "space");
-                arrRow.push("space");
-            } else {
-                arrRow.push(count);
-            }
-            newCol.setAttribute("value", count);
-            newCol.setAttribute("id", "cube" + count);
-            newCol.setAttribute("row", i);
-            newCol.setAttribute("col", g);
-            newRow.appendChild(newCol);
-        }
-        arr.push(arrRow);
-        table.appendChild(newRow);    
-    }
-    winStr = ""
-    MAIN_ARR = arr;
-    MAIN_ARR.forEach(el => {
-      winStr += el.join("");
-    });
-}
+  let count = 0;
+  let table = document.getElementById("myTable");
+  table.innerHTML = "";
+  let arr = [];
+  for (let i = 0; i < row; i++) {
+    let newRow = document.createElement("tr");
+    let arrRow = [];
+    newRow.setAttribute("id", "row" + (i + 1));
+    for (let g = 0; g < col; g++) {
+      count++;
 
+      let newCol = document.createElement("td");
+      newCol.innerText = count;
+      if (count == row * col) {
+        newCol.innerText = "";
+        newCol.setAttribute("class", "space");
+        arrRow.push("space");
+      } else {
+        arrRow.push(count);
+      }
+      newCol.setAttribute("value", count);
+      newCol.setAttribute("id", "cube" + count);
+      newCol.setAttribute("row", i);
+      newCol.setAttribute("col", g);
+      newRow.appendChild(newCol);
+    }
+    arr.push(arrRow);
+    table.appendChild(newRow);
+  }
+  winStr = "";
+  MAIN_ARR = arr;
+  MAIN_ARR.forEach((el) => {
+    winStr += el.join("");
+  });
+};
 
 let spaceNeighbors = (i, j, arr) => {
   let myNeigs = [];
@@ -124,50 +152,43 @@ const checkIndex = (i, j, arr) => {
   return true;
 };
 
-
-
-document.addEventListener('click', (event) => {
+document.addEventListener("click", (event) => {
   const isClickable = event.target.nodeName === "TD";
   if (isClickable) {
+    let currEl = event.target;
+    let currElValue = parseInt(currEl.getAttribute("value"));
+    let spaceEl = document.getElementsByClassName("space")[0];
+    let neighbors = spaceNeighbors(
+      spaceEl.getAttribute("row"),
+      spaceEl.getAttribute("col"),
+      MAIN_ARR
+    );
 
-      let currEl = event.target;
-      let currElValue = parseInt(currEl.getAttribute("value"));
-      let spaceEl = document.getElementsByClassName("space")[0];
-      let neighbors = spaceNeighbors(spaceEl.getAttribute("row"), spaceEl.getAttribute("col"), MAIN_ARR);
-
-      if(neighbors.includes(currElValue)){
-
-        swapNeighbor(spaceEl, currEl);
-        if(winCheck()){
-          alert("You won")
-        }
-      } else {
-      
-        // alert("Can't swap elements that are not neighbors with the space");
-
+    if (neighbors.includes(currElValue)) {
+      swapNeighbor(spaceEl, currEl);
+      if (winCheck()) {
+        alert("You won");
       }
-     
+    } else {
+      // alert("Can't swap elements that are not neighbors with the space");
+    }
   }
-
 });
 document.getElementById("shuffle").style.visibility = "hidden";
 
-  document.getElementById("startBtn").addEventListener('click', () => {
-    console.log("Click the btn!");
-    let rows = parseInt(document.getElementById("rows").value);
-    let cols = parseInt(document.getElementById("cols").value);
-    if(rows === NaN || cols === NaN){
-      alert("Rows and Cols have to be a integer number.");
-    } else {
-      tableCreator(rows, cols);
-      document.getElementById("shuffle").style.visibility = "visible";
-    }
+document.getElementById("startBtn").addEventListener("click", () => {
+  console.log("Click the btn!");
+  let rows = parseInt(document.getElementById("rows").value);
+  let cols = parseInt(document.getElementById("cols").value);
+  if (rows === NaN || cols === NaN) {
+    alert("Rows and Cols have to be a integer number.");
+  } else {
+    tableCreator(rows, cols);
+    pictureChanger("400.png")
+    document.getElementById("shuffle").style.visibility = "visible";
+  }
+});
 
-  });
-
-  document.getElementById("shuffle").addEventListener('click', () => {
-    shuffleTable();
-  })
-
-
-
+document.getElementById("shuffle").addEventListener("click", () => {
+  shuffleTable();
+});
